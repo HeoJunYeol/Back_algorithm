6811c1feff504c866a2d09ddfadc18309c27b242
HeoJunYeol <wnsduf9160@naver.com>
2022-05-24 23:22:23 +0900

    2020_05_24

--- 5719.py
+++ 5719.py
@@ -1,6 +1,5 @@
 from sys import *
 import heapq
-t = int(stdin.readline())
 
 def dij(start):
     distances = {node: float('inf') for node in dijkstra}
@@ -8,12 +7,12 @@ def dij(start):
     queue = []
     heapq.heappush(queue, [distances[start],start])
     while queue:
-        current_distance, current_destnation = heapq.heappop(queue)
+        current_distance, current_destination = heapq.heappop(queue)
 
-        if distances[current_destnation] < current_distance:
+        if distances[current_destination] < current_distance:
             continue
 
-        for new_destination, new_distance in dijkstra[current_destnation]:
+        for new_destination, new_distance in dijkstra[current_destination]:
             distance = current_distance + new_distance
             if distance < distances[new_destination] :
                 distances[new_destination] = distance
@@ -21,5 +20,15 @@ def dij(start):
 
     return distances
 
-for i in range(t):
-    n, m = map(int, stdin.readline().split())
+n, m = map(int, stdin.readline().split())
+
+while n != 0 and m != 0:
+    state = [i for i in range(n)]
+    s, d = map(int, stdin.readline().split())
+    dijkstra = {i:[] for i in range(n+1)}
+    for _ in range(m):
+        u, v, p = map(int, stdin.readline().split())
+        dijkstra[u] += [[v,p]]
+    dij(s)
+
+
